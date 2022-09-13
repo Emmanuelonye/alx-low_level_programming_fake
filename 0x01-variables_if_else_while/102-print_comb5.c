8d41070d9f3ebda98784ac1d34c5f9b608878358
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-12 21:31:55 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,15 +20,16 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
-				continue;
+		if (num1 == 98 && num2 == 99)
+		
+		continue;
 
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
+
 		}
 	}
-
 	
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
