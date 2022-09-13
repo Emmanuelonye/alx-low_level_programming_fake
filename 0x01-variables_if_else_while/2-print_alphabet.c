fa0aaeae106e7bf00d7cc47cf22fccb00d6a335e
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-12 18:17:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch1;
+
+	for (ch1 = 'a'; ch1 <= 'z'; ch1++)
+		putchar(ch1);
+		printf("\n");
+
+	return (0);
+}
