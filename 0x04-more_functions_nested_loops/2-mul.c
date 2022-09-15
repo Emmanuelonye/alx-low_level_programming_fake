8664948f97c2a8f33224d84c09584aa10fe14bcb
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-15 16:35:49 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 
 /**
  * mul - Multiplies two integers
- * @a - The first number to multiply
- * @b - The second number to multiply with first number
+ * @a : The first number to multiply
+ * @b : The second number to multiply with first number
  * Return: The result of multiplication between a and b
  */
 int mul(int a, int b)
