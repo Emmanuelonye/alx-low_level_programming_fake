6b932ee0c167a4211247a4bfba64e10e72cbee91
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-18 13:07:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -  prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return - Always 0(success)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
