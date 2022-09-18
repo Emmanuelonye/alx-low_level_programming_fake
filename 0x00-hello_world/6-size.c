f5b9a3cf9e154eac230e586a26f9f5805a400801
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-18 14:31:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *	main - Entry point
+ *	Return - Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char ch1;
+	int i;
+	long int c;
+	long long int x;
+	float f;
+
+	printf("Size of char:%lu byte(s)\n", sizeof(ch1));
+	printf("Size of an int:%lu byte(s)\n", sizeof(i));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(x));
+	printf("Size of a float:%lu byte(s)", sizeof(f));
+	return (0);
+}
