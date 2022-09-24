0a60ec386627e142c05f3c4b815f0f452659cd71
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-24 14:09:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * return : always
+ *
+int _abs(int)
+{
+	int c;
+
+	if (c >= 0)
+	{
+		_putchar(c);
+	}
+	
+	int i;
+
+	i = - * c;
+	if (c < 0)
+	{
+		_putchar(i);
+	}
+	return (0);
+
+}
