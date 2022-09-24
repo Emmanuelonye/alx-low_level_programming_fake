7c9e015016ddcdb7624c812b841cb880ace6433c
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-24 00:52:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry pount.
+ *
+ * Return : Always 0.
+ */
+int main ()
+{
+	char c;
+	c = 'a';
+	while (c = 'z')
+	{
+	putchar(%c, c);
+	c++;
+	}
+	putchar("\n");
+	return (0);
+}
