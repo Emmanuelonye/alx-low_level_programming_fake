69c6cdf378b798ff84f35a16566eaa1a4ef733ed
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-25 12:26:26 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print 1-100, replace multiples of 3/5/both w/ Fizz/Buzz/FizzBuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		printf("Fizz");
+		else if (i % 5 == 0)
+		printf("Buzz");
+		else if (i % 15 == 0)
+		printf("FizzBuzz");
+		else 
+		printf("%d", i);
+		if (i != 100)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
