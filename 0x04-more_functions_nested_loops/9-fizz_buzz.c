d76b4d2cda3ad119eb1ae58eb7f2344fcfdb9d34
Emmanuel Nnabugwu <nnabugwuemmanuel675@gmail.com>
2022-09-25 12:32:25 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
@@ -17,7 +17,7 @@ int main(void)
 		printf("Buzz");
 		else if (i % 15 == 0)
 		printf("FizzBuzz");
-		else 
+		else
 		printf("%d", i);
 		if (i != 100)
 		printf(" ");
